168a06689e7ad91e402c28b80793f5898cdf9c14
linrium <linrium@gmail.com>
2026-02-01 12:01:38 +0900

    refactor: Remove authentication logic, sign-out form, and navigation elements from the dashboard page.

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,40 +1,13 @@
-import { headers } from "next/headers"
-import Link from "next/link"
-import { redirect } from "next/navigation"
 import { Suspense } from "react"
-import { Button } from "@/components/ui/button"
-import { createPost, getPosts } from "@/lib/actions/posts"
-import { auth } from "@/lib/auth"
+import { getPosts } from "@/lib/actions/posts"
 import CreatePostForm from "./_components/create-post-form"
 import Posts from "./_components/posts"
 
 export default async function Page() {
-  const session = await auth.api.getSession({
-    headers: await headers(),
-  })
-
   const posts = getPosts()
 
   return (
     <div>
-      <Link href="/">Home</Link>
-      <h1>Dashboard</h1>
-      {session?.user?.email}
-      <form
-        action={async () => {
-          "use server"
-          await auth.api.signOut({
-            headers: await headers(),
-          })
-
-          redirect("/sign-in")
-        }}
-      >
-        <Button type="submit" variant="destructive">
-          Sign Out
-        </Button>
-      </form>
-
       <Suspense fallback={<div>Loading...</div>}>
         <Posts posts={posts} />
       </Suspense>
